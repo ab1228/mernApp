d1e53ef49b373690c109429dd7a9038cfd00ec15
ab1228 <alexb8734@gmail.com>
2020-03-21 16:19:33 -0500

    updated server and added models

--- server.js
+++ server.js
@@ -1,10 +1,22 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const morgan = require('morgan');
 const path = require('path');
 
 const PORT = process.env.PORT || 8070;
 
 var app = express();
+
+app.use(morgan('tiny'));
+
+mongoose.connect("mongodb://localhost/mernApp", {
+    useNewUrlParser: true, useUnifiedTopology: true
+});
+/// TO CHECK IF MONGOOSE HAS BEEN CONNECTED
+mongoose.connection.on('connected', () => {
+    console.log('mongoose is connected')
+})
+var db = require("./models");
 
 
 
