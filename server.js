54ef8f1fd4e63cb0e0efbed059c673465c4b308a
Alex Blanco <alexb8734@gmail.com>
2020-04-05 02:24:17 -0500

    updated

--- server.js
+++ server.js
@@ -26,8 +26,14 @@ mongoose.connection.on('connected', () => {
 
 app.use('/api', routes);
 
+
+//SERVE STATIC ASSETS
 if (process.env.NODE_ENV === 'production') {
     app.use(express.static('client/build'));
+
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    });
 
 }
 
